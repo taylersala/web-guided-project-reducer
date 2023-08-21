2d80813886d9e5246524e6817537404c3603874a
taylersala <taylersala@gmail.com>
2023-08-20 17:28:55 -0700

    toggleEdit not defined

---
+++ followAlong/src/actions/title.actions.js
@@ -0,0 +1,17 @@
+export const SET_TITLE = "SET_TITLE";
+export const TOGGLE_EDITING = "TOGGLE_EDITING";
+export const SET_NEW_TEXT_TITLE = "SET_NEW_TEXT_TITLE";
+
+//const [state, dispatch] = useReducer(reducer, initialState);
+
+export const setTitle = (title) => {
+    return {type: SET_TITLE, payload: title};
+}
+
+export const toggleEditing = () => {
+    return {type: TOGGLE_EDITING};
+}
+
+export const setNewTitleText = (newTitleText) => {
+    return {type: SET_NEW_TEXT_TITLE, payload: newTitleText};
+}
